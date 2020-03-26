bbbfa9a9596b6369e0c22ea3d0013620fab7c2d6
emlia <771631347@qq.com>
2020-03-27 00:38:41 +0800

    modify tag a style

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 import hljs from 'highlight.js'
-import 'highlight.js/styles/github.css'
+import 'highlight.js/styles/rainbow.css'
 
 const highlightCode = () => {
   const preEl = document.querySelectorAll('pre')
@@ -34,5 +34,20 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+a {
+  text-decoration: none;
+  background-color: #00a0e9;
+  border-radius: 4px;
+  padding: 2px 10px;
+  color: #fff;
+
+  transition: all ease 0.3s;
+  &:hover {
+    background-color: rgba(0, 160, 233, 0.7);
+  }
+  &:visited {
+    color: #fff;
+  }
 }
 </style>
