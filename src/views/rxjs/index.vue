d8ded960fa3e789e00f2b9f89d6eb54a1fed7746
emlia <771631347@qq.com>
2020-03-28 22:48:47 +0800

    modify demo.html

--- src/views/rxjs/index.vue
+++ src/views/rxjs/index.vue
@@ -1,16 +1,10 @@
 <template>
   <div>
-    <!-- <span>this rxjs page</span>
-    <div v-for="link in links" :key="link.name">
-      <span>{{link.name}}</span>
-      <show-html :url="link.path"></show-html>
-    </div> -->
     <rxjs></rxjs>
   </div>
 </template>
 
 <script>
-import ShowHtml from '@/components/show-html'
 import Rxjs from '@note/rxjs/README.md'
 export default {
   components: { Rxjs },
